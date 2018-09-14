8c8998dd7125bb79d67483152323da589851012c
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-09-14 18:39:56 +0200

    Menu og headerbaggrund opdateret

--- test_2 - nyt grid/script.js
+++ test_2 - nyt grid/script.js
@@ -1,27 +1,31 @@
 function onLoad() {
 
-	// if (window.innerWidth <= 768) {
+	console.log("onLoad");
 
-	function toggleMenu() {
-		document.querySelector(".burger").classList.toggle("change");
-		document.querySelector("nav").classList.toggle("show");
-		document.querySelector(".logo").classList.toggle("hide");
-		document.querySelector(".darkwrapper").classList.toggle("high");
+	if (window.innerWidth <= 768) {
+
+		function toggleMenu() {
+			console.log("toggleMenu");
+			document.querySelector(".burger").classList.toggle("change");
+			document.querySelector("nav").classList.toggle("show");
+			document.querySelector(".logo").classList.toggle("hide");
+			document.querySelector(".darkwrapper").classList.toggle("high");
+		}
+
+		function closeMenu() {
+			console.log("closeMenu");
+			document.querySelector(".burger").classList.remove("change");
+			document.querySelector("nav").classList.remove("show");
+			document.querySelector(".logo").classList.remove("hide");
+			document.querySelector(".darkwrapper").classList.remove("high");
+		}
+
+		document.querySelector(".burger").addEventListener("click", toggleMenu);
+		document.querySelector("nav ul").addEventListener("click", toggleMenu);
+		document.querySelector("nav ul").addEventListener("mouseleave", closeMenu);
+
 	}
-
-	function closeMenu() {
-		document.querySelector(".burger").classList.remove("change");
-		document.querySelector("nav").classList.remove("show");
-		document.querySelector(".logo").classList.remove("hide");
-		document.querySelector(".darkwrapper").classList.remove("high");
-	}
-
-	document.querySelector(".burger").addEventListener("click", toggleMenu);
-	document.querySelector("nav ul").addEventListener("click", toggleMenu);
-	document.querySelector("nav ul").addEventListener("mouseleave", closeMenu);
-
 }
-//}
 
 document.addEventListener("DOMContentLoaded", function (event) {
 	onLoad();
